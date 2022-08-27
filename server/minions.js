52c75082fdbf4b553b680818e458eb08a5498125
Richard Lovell <cassh3rn@talktalk.net>
2022-08-27 20:48:10 +0100

    add first get and post routes

--- server/minions.js
+++ server/minions.js
@@ -12,5 +12,15 @@ const {
     deleteAllFromDatabase,
   } = require('./db.js');
 
+router.get('/', (req, res,) => {
+    const allMinions = getAllFromDatabase('minions');
+    res.status(200).send(allMinions);
+});
+
+router.post('/', (req, res) => {
+  const newMinion = addToDatabase('minions', req.body);
+  res.status(201).send(newMinion);
+});
+
 
 module.exports = router; 
