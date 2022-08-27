30a9d1fdf990a0a5dabbfabb1715957afef41c87
Richard Lovell <cassh3rn@talktalk.net>
2022-08-27 18:27:18 +0100

    add minions.js file and set up import/export

---
+++ server/minions.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = require('../server');
+const router = express.Router();
+
+const {
+    createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase,
+  } = require('./db.js');
+
+
+module.exports = router; 
